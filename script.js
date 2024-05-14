31a9ed5811d41dc16ad17b1c3badd28fe492a195
Thialy Lima <thialymedeiros@gmail.com>
2024-05-14 17:33:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function toggleSelection(element) {
+    element.classList.toggle('selected');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Array de nomes de famosos
+    var nomesFamosos = [
+      "Xuxa",
+      "Cirilo",
+      "Matuê",
+      "Zeca",
+      "Estevão Ferreira",
+      "ET Bilu",
+      "Miranha",
+      "Pabllo Vittar",
+      "Bolsonaro",
+      "Lula",
+      "Chico Moedas",
+      "Casimiro",
+      "Marília Mendonça",
+      "Wagner Moura",
+      "Alanzoka",
+      "Batman",
+      "Araújo",
+      "Vanessa",
+      "Manoel Gomes",
+      "Muzy",
+      "Tiringa",
+      "Jesus",
+      "Hitler",
+      "Tereza"
+    ];
+  
+    // Seleciona um nome aleatório do array
+    var nomeAleatorio = nomesFamosos[Math.floor(Math.random() * nomesFamosos.length)];
+  
+    // Atualiza o texto na página HTML
+    document.getElementById("nomeFamoso").textContent = nomeAleatorio;
+  });
+  
